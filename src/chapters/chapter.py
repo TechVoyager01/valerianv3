9d756b231259fd306de39a7ef027adac2326c794
DaeWave <Damian.lousteau@ou.ac.uk>
2024-07-29 13:58:12 +0700

    adding chapters - still inprogress

---
+++ src/chapters/chapter.py
@@ -0,0 +1,29 @@
+# Storyline
+chapter_1_story = """
+Chapter 1: The Call to Adventure
+In the bustling capital city of Galador, Valerian hears rumors about the ancient city of Thaemus and its legendary artifact that grants eternal youth. 
+Driven by curiosity and ambition, he decides to embark on a quest to uncover the secrets of Thaemus. 
+Before leaving, he gathers supplies and meets an old sage who gives him a mystical map and some cryptic advice.
+You are sent on you way to discover the next clue....
+"""
+
+chapter_2_story = """
+Chapter 2: The Path of Trials
+Valerian ventures into the dense and mystical Enchanted Forest surrounding Thaemus. 
+He faces various challenges and encounters that test his strength, intelligence, and courage. 
+During his journey, he befriends a magical creature who aids him in overcoming the forest's dangers.
+"""
+
+chapter_3_story = """
+Chapter 3: The Treacherous Mountains
+Valerian continues his journey through the rugged and perilous Treacherous Mountains. 
+He navigates through difficult terrain, environmental hazards, and formidable enemies. 
+Along the way, he uncovers more about the history of Thaemus and its artifact.
+"""
+
+chapter_4_story = """
+Chapter 4: The Ruins of Thaemus
+Valerian finally reaches the ancient and forgotten city of Thaemus. 
+He must navigate through the ruins, solve intricate puzzles, and defeat powerful guardians to reach the inner sanctum where the artifact is kept. 
+The final confrontation tests Valerian's resolve and worthiness to obtain the artifact.
+"""
--- src/main.py
+++ src/main.py
@@ -1,17 +1,15 @@
 # imports of libraries
 import os
-from collections import ChainMap
-
-# import random
+import random
 
 # imports of functions
 from game_title import display_game_title
-# from destination_class import Destination
 from travel import *
+from enemies import *
+from chapters.chapter import *
 
 # variables
 gameplay = True
-play = True
 
 # player stats
 player_stats = {
@@ -23,154 +21,49 @@ player_stats = {
     'Gold': 0,
     'location': 'Galador',
 }
-# enemies and their description
-enemy_list = [
-        {
-            'id': 1,
-            "name": "Stone Sentinel",
-            "description": "Ancient golems created to guard Thaemus, now crushing intruders.",
-            "appearance": "Massive figures made of stone and metal with glowing runes.",
-            "abilities": "Immense strength, hurl boulders, create seismic shockwaves.",
-            "weakness": "Slow, susceptible to agility-based attacks and magic disrupting runes.",
-            "HP": 40,
-            "Attack": 10,
-            "Defence": 5,
-            "Gold": 20,
-            "Potion": 2
-        },
-        {
-            'id': 2,
-            "name": "Arcane Trickster",
-            "description": "Rogue mages using forbidden magic, deceiving and trapping adventurers.",
-            "appearance": "Hooded figures with glowing eyes, adorned with talismans.",
-            "abilities": "Illusions, powerful spells, magical traps, summon spectral minions.",
-            "weakness": "Overconfidence, illusions dispelled with clarity spells.",
-            "HP": 50,
-            "Attack": 15,
-            "Defence": 6,
-            "Gold": 40,
-            "Potion": 3
-        },
-        {
-            'id': 3,
-            "name": "Shadow Wraith",
-            "description": "Ghostly remnants of Thaemus's cursed inhabitants, haunting the ruins.",
-            "appearance": "Ghostly figures with glowing eyes and tattered cloaks.",
-            "abilities": "Phase through walls, become invisible, drain life force.",
-            "weakness": "Vulnerable to light-based magic and enchanted weapons.",
-            "HP": 60,
-            "Attack": 20,
-            "Defence": 7,
-            "Gold": 50,
-            "Potion": 4
-        },
-        {
-            'id': 4,
-            "name": "Forest Stalker",
-            "description": "Predatory creatures that camouflage and ambush travelers in the forests.",
-            "appearance": "Sleek, "
-                          "eline-like with mottled fur, sharp claws, piercing eyes.",
-            "abilities": "Stealth, climb trees, paralyzing bite.",
-            "weakness": "Vulnerable to fire and loud noises.",
-            "HP": 70,
-            "Attack": 25,
-            "Defence": 7,
-            "Gold": 60,
-            "Potion": 5
-        }
-    ]
 
 # functions
-# function to display a line
 def draw_line():
+    """Function to print a decorative line."""
     print("xX--------------------------------------Xx")
 
-# function to clear the terminal screen
 def clear_terminal():
-    # Check the operating system and execute the appropriate command
-    if os.name == 'nt':  # For Windows
+    """Function to clear the terminal screen based on the operating system."""
+    if os.name == 'nt':
         os.system('cls')
-    else:  # For macOS and Linux
+    else:
         os.system('clear')
 
-# function to display the game title
 def display_game_title_with_lines():
+    """Function to display the game title with decorative lines."""
     draw_line()
     display_game_title()
     draw_line()
 
-# function to display the story line and what to encounter
 def display_story():
+    """Function to display the introductory story of the game."""
     intro_text = """
     Welcome, brave adventurer, to the mystical world of Orius!
-
-    In the heart of Orius lies the bustling city of Galador,
-    where stories of ancient legends and forgotten treasures
-    are whispered in every tavern and marketplace.
-    Among these legends, one tale stands out above the rest—
-    the legend of Thaemus, a once-great city now lost to time and myth.
-
-    Centuries ago, Thaemus was the crown jewel of Orius,
-    a city of unparalleled beauty and prosperity.
-    Its citizens lived in peace and harmony,
-    protected by a powerful artifact that granted eternal youth
-    to those deemed worthy.
-    However, as time passed, greed and jealousy grew
-    within the hearts of the people.
-    In their desperation to control the artifact's power,
-    a curse befell the city, and it vanished into the shadows,
-    hidden away from the world.
-
-    You are Valerian, a young and curious explorer from Galador.
-    Driven by tales of Thaemus and the promise of eternal youth,
-    you decide to embark on a daring quest
-    to uncover the secrets of the lost city.
-    Your journey will take you through dense forests,
-    treacherous mountains, and perilous ruins,
-    where you will face trials that test your strength,
-    wit, and courage.
-
-    Your ultimate goal is to find the ancient artifact of Thaemus
-    and prove yourself worthy of its power.
-    But beware—the path is fraught with danger,
-    and only the bravest and most cunning adventurers will succeed.
-    Along the way, you will uncover the true history of Thaemus,
-    meet allies who will aid you,
-    and confront formidable foes who will stop at nothing
-    to protect the secrets of the city.
-
-    Are you ready to take up the challenge
-    and embark on the quest of a lifetime?
-    The fate of Thaemus and the promise of eternal youth await you!
-
-    1. Prepare Yourself: Gather your wits and steel your nerves
-       for the journey ahead. The path to Thaemus is not for the faint of heart.
-    2. Meet the Sage: Seek out the old sage in Galador
-       who holds the key to your quest.
-       He will provide you with a mystical map and cryptic advice to guide you on your way.
-    3. Embark on Your Journey: Leave the safety of Galador
-       and venture into the wild, where trials and challenges await.
-       Trust in your skills and the friends you meet along the way.
-
+    ...
     Press Enter to begin your adventure...
     """
     print(intro_text)
 
-# function to print player stats updates in battle
 def print_player_stats(player_stats):
+    """Function to print the player's current stats."""
     print("\nValerian Stats:")
     for key, value in player_stats.items():
         print(f"{key}: {value}")
 
-# function to display enemy stats in battle
 def print_enemy_stats(enemy):
+    """Function to print the current enemy's stats."""
     print("\nEnemy Stats:")
     for key, value in enemy.items():
-        if key not in ['id', 'name', 'description', 'appearance', 'abilities', 'weakness']:
+        if key not in ['id', 'name', 'description', 'appearance', 'abilities', 'weakness', 'location', 'encounter', 'chapter']:
             print(f"{key}: {value}")
 
-# function to reset player stats
 def reset_player_stats():
+    """Function to reset the player's stats to the initial values."""
     return {
         'Health': 100,
         'Attack': 10,
@@ -178,11 +71,11 @@ def reset_player_stats():
         'Potion': 3,
         'Elixir': 1,
         'Gold': 0,
-        'location': 'Galador',
+        'location': 'Galador\n',
     }
 
-# function to display the user's choice, load game, new game, save and exit
 def display_choice():
+    """Function to display the main menu choices."""
     draw_line()
     print(
         'Select an option: \n',
@@ -192,35 +85,18 @@ def display_choice():
     )
     draw_line()
 
-# function to display the battle action options, fight, defend, use potion, use elixir, run
 def battle_action():
+    """Function to display the battle action choices."""
     print('Battle Action')
     print(
         '\nAttack: 1',
         '\nDefend: 2',
         '\nUse Potion (20HP): 3',
-        '\nUse Elixir (40HP): 4',
-        '\nRun: 4',
+        '\nUse Elixir (40HP): 4\n',
     )
 
-# function to display the travel options, north, south, east, west  in development for later gameplay
-# def print_travel_choice():
-#     print('Travel Options')
-#     print(
-#         'North: 1\n',
-#         'South: 2\n',
-#         'East: 3\n',
-#         'West: 4\n',
-#         'Return to Galador: 5\n',
-#     )
-
-# function to make a random number between 0 and 10 to call battle function
-# def random_number():
-#     return random.randint(0, 10)
-
-# function to display enemies
-
 def display_enemies():
+    """Function to display the list of enemies."""
     for enemy in enemy_list:
         print(f"Name: {enemy['name']}")
         print(f"Description: {enemy['description']}")
@@ -235,6 +111,7 @@ def display_enemies():
         print("-" * 40)
 
 def display_single_enemy(enemy):
+    """Function to display a single enemy's details."""
     print(f"Name: {enemy['name']}")
     print(f"Description: {enemy['description']}")
     print(f"Appearance: {enemy['appearance']}")
@@ -247,9 +124,69 @@ def display_single_enemy(enemy):
     print(f"Potion: {enemy['Potion']}")
     print("-" * 40)
 
-# maybe put into own file later
-# function to save player stats to load.txt
+def battle_option_enemy(enemy):
+    """Function to handle the enemy's battle actions."""
+    random_num = random.randint(1, 4)
+    if enemy['Health'] > 0:
+        if random_num == 1:
+            print('Your enemy has chosen to attack')
+            print(f"The {enemy['name']} attacks you!")
+            player_stats['Health'] -= enemy['Attack'] - player_stats['Defence']
+            print(f"Your health is now {player_stats['Health']}")
+        elif random_num == 2:
+            print(f"The {enemy['name']} defends against you!")
+            enemy['Defence'] += 5
+            print(f"The {enemy['name']} increased their defence to {enemy['Defence']}.")
+        elif random_num == 3:
+            if enemy['Potion'] > 0:
+                print('Your enemy has chosen to use a potion')
+                enemy['Health'] += 20
+                enemy['Potion'] -= 1
+                print(f"The {enemy['name']} used a potion. They have {enemy['Potion']} potions left.")
+            else:
+                print('The enemy has no potions left!')
+        elif random_num == 4:
+            if enemy['Elixir'] > 0:
+                print('Your enemy has chosen to use an elixir')
+                enemy['Health'] += 40
+                enemy['Elixir'] -= 1
+                print(f"The {enemy['name']} used an elixir. They have {enemy['Elixir']} elixirs left.")
+            else:
+                print('The enemy has no elixirs left!')
+    else:
+        print('There is a problem in the logic of the enemy fighting you')
+
+def battle_option_player(player_stats, enemy):
+    """Function to handle the player's battle actions."""
+    choice = input('Choose your action: ')
+    clear_terminal()
+
+    if choice == '1':
+        print(f"You attack the {enemy['name']}!")
+        enemy['Health'] -= player_stats['Attack']
+        print(f"The {enemy['name']} has {enemy['Health']} HP left.")
+    elif choice == '2':
+        print("You defend against the next attack.")
+        player_stats['Defence'] += 5
+    elif choice == '3':
+        if player_stats['Potion'] > 0:
+            player_stats['Health'] += 20
+            player_stats['Potion'] -= 1
+            print("You used a potion. Your health is now", player_stats['Health'])
+        else:
+            print("You have no potions left!")
+    elif choice == '4':
+        if player_stats['Elixir'] > 0:
+            player_stats['Health'] += 40
+            player_stats['Elixir'] -= 1
+            print("You used an elixir. Your health is now", player_stats['Health'])
+        else:
+            print("You have no elixirs left!")
+    else:
+        print("Invalid choice. Please select a valid action.")
+
 def save_player_stats(file_path, player_stats):
+    """Function to save the player's stats to a file."""
     try:
         with open(file_path, 'w') as file:
             for key, value in player_stats.items():
@@ -258,8 +195,8 @@ def save_player_stats(file_path, player_stats):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# function to load player stats from load.txt
 def load_player_stats(file_path):
+    """Function to load the player's stats from a file."""
     try:
         with open(file_path, 'r') as file:
             for item in file:
@@ -272,94 +209,93 @@ def load_player_stats(file_path):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# function to call the battle function
 def battle(enemy_list, player_stats):
-    # Select the first enemy for the battle
-    enemy = enemy_list[0]
-    draw_line()
-    print('You are at battle with', enemy['name'])
-    draw_line()
-    # battle loop
-    while player_stats['Health'] > 0 and enemy['HP'] > 0:
+    """Function to handle the battle sequence between the player and enemies."""
+    while player_stats['Health'] > 0 and len(enemy_list) > 0:
         enemy = enemy_list[0]
-        # functions to display player and enemy stats
+        enemy_encounter = enemy["encounter"]
         draw_line()
-        print_player_stats(player_stats)
+        print(enemy_encounter)
         draw_line()
-        print_enemy_stats(enemy)
-        draw_line()
-        # function to display battle action options
-        battle_action()
+        print('You are at battle with', enemy['name'])
         draw_line()
 
-        # Get player's choice
-        choice = input('Choose your action: ')
-        clear_terminal()
+        while player_stats['Health'] > 0 and enemy['Health'] > 0:
+            draw_line()
+            print_player_stats(player_stats)
+            draw_line()
+            print_enemy_stats(enemy)
+            draw_line()
+            battle_action()
+            draw_line()
 
-        if choice == '1':  # Attack
-            print(f"You attack the {enemy['name']}!")
-            enemy['HP'] -= player_stats['Attack']
-            print(f"The {enemy['name']} has {enemy['HP']} HP left.")
+            battle_option_player(player_stats, enemy)
+            battle_option_enemy(enemy)
 
-        elif choice == '2':  # Defend
-            print("You defend against the next attack.")
-            player_stats['Defence'] += 5  # Temporary defense boost
+        if player_stats['Health'] <= 0:
+            clear_terminal()
+            print("You have been defeated!")
+            main_gameplay()
+            return
+        elif enemy['Health'] <= 0:
+            clear_terminal()
+            print(f"You have defeated the {enemy['name']}!")
+            player_stats['Gold'] += enemy['Gold']
+            enemy_list.pop(0)
 
-        elif choice == '3':  # Use Potion
-            if player_stats['Potion'] > 0:
-                player_stats['Health'] += 20
-                player_stats['Potion'] -= 1
-                print("You used a potion. Your health is now", player_stats['Health'])
-            else:
-                print("You have no potions left!")
+    if len(enemy_list) == 0:
+        print("Congratulations! You have defeated all enemies! \nOnto the next chapter!")
+        next_chapter(1)
 
-        elif choice == '4':  # Use Elixir
-            if player_stats['Elixir'] > 0:
-                player_stats['Health'] += 40
-                player_stats['Elixir'] -= 1
-                print("You used an elixir. Your health is now", player_stats['Health'])
-            else:
-                print("You have no elixirs left!")
+def update_player_location(player_stats, chapter):
+    """Function to update the player's location based on the current chapter."""
+    chapter_locations = {
+        1: 'Galador',
+        2: 'Enchanted Forest',
+        3: 'Treacherous Mountains',
+        4: 'Ruins of Thaemus'
+    }
+    player_stats['location'] = chapter_locations.get(chapter, 'Unknown Location')
 
-        else:
-            print("Invalid choice. Please select a valid action.")
-            continue
+def next_chapter(current_chapter):
+    """Function to transition to the next chapter after defeating all enemies."""
+    print("You have completed the first chapter. Prepare for the next chapter!")
+    global player_stats, enemy_list
+    player_stats['Health'] = 100
+    player_stats['Potion'] += 2
+    player_stats['Elixir'] += 1
+    current_chapter += 1
+    update_player_location(player_stats, current_chapter)
+    print(f"Your new location is {player_stats['location']}")
 
-        # Enemy's turn to attack
-        if enemy['HP'] > 0:
-            print(f"The {enemy['name']} attacks you!")
-            player_stats['Health'] -= enemy['Attack'] - player_stats['Defence']
-            print(f"Your health is now {player_stats['Health']}")
-
-        # Reset temporary defense boost
-        if choice == '2':
-            player_stats['Defence'] -= 5
-
-    # Check win/lose conditions
-    if player_stats['Health'] <= 0:
-        clear_terminal()
-        print("You have been defeated!")
-        main_gameplay()  # Restart the game if defeated
-    elif enemy['HP'] <= 0:
-        clear_terminal()
-        print(f"You have defeated the {enemy['name']}!")
-
-# temp glador encounter
 def galador():
+    """Function to start the Galador encounter."""
     clear_terminal()
-    # change up the start but for now just make it work
-    print("Welcome to Galador, the bustling capital city."
-          "\nYour journey begins now..."
-          "\nYou must defeat the four foes a head of you....."
-          "\nYour battle begins now!"
-          "\nFight!!!")
-    battle(enemy_list, player_stats)
+    print(chapter_1_story)
+    battle(chapter_1_enemies, player_stats)
 
+def enchanted_forest():
+    """Function to start the Enchanted Forest encounter."""
+    clear_terminal()
+    print(chapter_2_story)
+    battle(chapter_2_enemies, player_stats)
 
-# main
+def treacherous_mountains():
+    """Function to start the Treacherous Mountains encounter."""
+    clear_terminal()
+    print(chapter_3_story)
+    battle(chapter_3_enemies, player_stats)
+
+def ruins_of_thaemus():
+    """Function to start the Ruins of Thaemus encounter."""
+    clear_terminal()
+    print(chapter_4_story)
+    battle(chapter_4_enemies, player_stats)
+
 def main_gameplay():
+    """Main function to handle the gameplay loop."""
     global player_stats
-    player_stats = reset_player_stats()  # Reset player stats at the start of the game
+    player_stats = reset_player_stats()
 
     while gameplay:
         clear_terminal()
