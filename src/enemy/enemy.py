a1baddaf6507c65f86f2a23ebfbcb17d1c0b546c
DaeWave <Damian.lousteau@ou.ac.uk>
2024-07-31 21:45:30 +0700

    spelling error

--- src/enemy/enemy.py
+++ src/enemy/enemy.py
@@ -1,11 +1,13 @@
 # making a enemy class to easily create future enemies for valerian to fight
 # you can take this class and add enemies to the enemies list and create chapters around these enemies
+# i added a defeat object so that when the foe is defeated you can display a message about this
 class Enemy:
-    def __init__(self, id, encounter, name, description, appearance, abilities, weakness, location, chapter, health, attack, defence, gold, potion, elixir):
+    def __init__(self, id, encounter, name, description, defeat, appearance, abilities, weakness, location, chapter, health, attack, defence, gold, potion, elixir):
         self.id = id
         self.encounter = encounter
         self.name = name
         self.description = description
+        self.defeat = defeat
         self.appearance = appearance
         self.abilities = abilities
         self.weakness = weakness
@@ -26,11 +28,12 @@ class Enemy:
 #     id=1,
 #     encounter='In a dark alley, you encounter a rough-looking individual with a crude weapon. It is a Street Thug, a common criminal lurking in the shadows.',
 #     name="Street Thug",
-#     description="Common criminals lurking in the alleys of Galador.",
+#     description="Common criminals lurking in the alleys of Glador.",
+#     defeat="The Street Thug falls to the ground, defeated.",
 #     appearance="Rough-looking individuals with crude weapons.",
 #     abilities="Quick attacks, can call for backup.",
 #     weakness="Low health, susceptible to strong single attacks.",
-#     location="Galador",
+#     location="Glador",
 #     chapter=1,
 #     health=20,
 #     attack=5,
